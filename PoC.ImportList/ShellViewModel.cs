54b13a1be4da93f736b0e84463da6bfe4769213c
Kris McGinnes <kmcginnes@me.com>
2014-09-14 15:19:28 -0500

    Added window title

--- PoC.ImportList/ShellViewModel.cs
+++ PoC.ImportList/ShellViewModel.cs
@@ -4,6 +4,11 @@ namespace PoC.ImportList
 {
     public class ShellViewModel : ReactiveConductor<ImportViewModel>, IShell
     {
+        public ShellViewModel()
+        {
+            DisplayName = "Import";
+        }
+
         protected override void OnActivate()
         {
             base.OnActivate();
