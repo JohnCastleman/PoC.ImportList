81073e6a3a30c942ceddb734e663a7cc5e13a171
Kris McGinnes <kmcginnes@me.com>
2014-09-14 15:04:24 -0500

    Fixed some styling

--- PoC.ImportList/ImportViewModel.cs
+++ PoC.ImportList/ImportViewModel.cs
@@ -13,8 +13,14 @@ namespace PoC.ImportList
             ViewContext = ViewContext.List;
             Items.AddRange(new[]
             {
-                new RecordedEventViewModel {Sequence = 1, OfficerName = "Bob Smith", Started = new DateTime(2014, 9, 14, 7, 45, 19)}, 
-                new RecordedEventViewModel {Sequence = 2, OfficerName = "John Gomer", Started = new DateTime(2014, 9, 15, 3, 04, 54)}, 
+                new RecordedEventViewModel {Sequence = 1, OfficerName = "Tom Landry", Started = new DateTime(1960, 9, 14, 7, 45, 19)}, 
+                new RecordedEventViewModel {Sequence = 2, OfficerName = "Jimmy Johnson", Started = new DateTime(1989, 9, 15, 3, 04, 54)}, 
+                new RecordedEventViewModel {Sequence = 3, OfficerName = "Barry Switzer", Started = new DateTime(1994, 9, 15, 3, 04, 54)}, 
+                new RecordedEventViewModel {Sequence = 4, OfficerName = "Chan Gailey", Started = new DateTime(1998, 9, 15, 3, 04, 54)}, 
+                new RecordedEventViewModel {Sequence = 5, OfficerName = "Dave Campo", Started = new DateTime(2000, 9, 15, 3, 04, 54)}, 
+                new RecordedEventViewModel {Sequence = 6, OfficerName = "Bill Parcels", Started = new DateTime(2003, 9, 15, 3, 04, 54)}, 
+                new RecordedEventViewModel {Sequence = 7, OfficerName = "Wade Phillips", Started = new DateTime(2007, 9, 15, 3, 04, 54)}, 
+                new RecordedEventViewModel {Sequence = 8, OfficerName = "Jason Gerrett", Started = new DateTime(2010, 9, 15, 3, 04, 54)}, 
             });
 
             if (Execute.InDesignMode)
