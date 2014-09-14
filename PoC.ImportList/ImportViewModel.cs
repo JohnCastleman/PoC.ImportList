16be2686626e4d8566d9ef92ed09c8de1a16c25e
Kris McGinnes <kmcginnes@me.com>
2014-09-14 14:36:45 -0500

    Switched to using view context

---
+++ PoC.ImportList/ImportViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Caliburn.Micro.ReactiveUI;
+using ReactiveUI;
+
+namespace PoC.ImportList
+{
+    public class ImportViewModel : ReactiveConductor<RecordedEventViewModel>.Collection.OneActive
+    {
+        public ImportViewModel()
+        {
+            ViewContext = ViewContext.List;
+            Items.AddRange(new[]
+            {
+                new RecordedEventViewModel {OfficerName = "Bob Smith", Started = new DateTime(2014, 9, 14, 7, 45, 19)}, 
+                new RecordedEventViewModel {OfficerName = "John Gomer", Started = new DateTime(2014, 9, 15, 3, 04, 54)}, 
+            });
+        }
+
+        ViewContext _viewContext;
+        public ViewContext ViewContext
+        {
+            get { return _viewContext; }
+            set { this.RaiseAndSetIfChanged(ref _viewContext, value); }
+        }
+    }
+
+    public enum ViewContext
+    {
+        List, Details
+    }
+}
--- PoC.ImportList/ShellViewModel.cs
+++ PoC.ImportList/ShellViewModel.cs
@@ -1,18 +1,13 @@
-using System;
 using Caliburn.Micro.ReactiveUI;
 
 namespace PoC.ImportList
 {
-    public class ShellViewModel : ReactiveConductor<RecordedEventViewModel>.Collection.OneActive, IShell
+    public class ShellViewModel : ReactiveConductor<ImportViewModel>, IShell
     {
-        public ShellViewModel()
+        protected override void OnActivate()
         {
-            Items.AddRange(new []
-            {
-                new RecordedEventViewModel {OfficerName = "Bob Smith", Started = new DateTime(2014, 9, 14, 7, 45, 19)}, 
-                new RecordedEventViewModel {OfficerName = "John Gomer", Started = new DateTime(2014, 9, 15, 3, 04, 54)}, 
-            });
+            base.OnActivate();
+            ActivateItem(new ImportViewModel());
         }
-
     }
 }
